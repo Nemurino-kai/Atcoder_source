9b6408adb783cfad5800ade296b3a76d930d83e9
DoubleZ <kohei.tashiro@chiba-u.jp>
2017-10-28 21:01:58 +0900

    [AC] abc076 abc076_a

---
+++ atcoder.jp/abc076/abc076_a/Main.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+#define REP(i,n) for(int i=0;i< (n);i++)
+#define REPR(i, n) for(int i = (n);i >= 0;i--)
+#define FOR(i, m, n) for(int i = (m);i < (n);i++)
+#define FORR(i, m, n) for(int i = (m);i >= (n);i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define pb(a) push_back(a)
+#define ALL(obj) (obj).begin(),(obj).end()
+
+using namespace std;
+
+const int INF = 99999999;
+const ll LINF = 9999999999999;
+
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+int vx[4] = { 0,1,0,-1 };
+int vy[4] = { 1,0,-1,0 };
+
+int a, b;
+
+signed main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	cin >> a >> b;
+
+	cout << a + (b - a) * 2;
+
+
+	return 0;
+
+}
