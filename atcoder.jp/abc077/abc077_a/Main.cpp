34fbac44ca1fc665832335a0559339ba2a65cbd4
DoubleZ <kohei.tashiro@chiba-u.jp>
2017-11-04 21:02:30 +0900

    [AC] abc077 abc077_a

---
+++ atcoder.jp/abc077/abc077_a/Main.cpp
@@ -0,0 +1,40 @@
+//ABC 077
+#include "bits/stdc++.h"
+#define REP(i,n) for(int i=0;i< (n);i++)
+#define REPR(i, n) for(int i = (n);i >= 0;i--)
+#define FOR(i, m, n) for(int i = (m);i < (n);i++)
+#define FORR(i, m, n) for(int i = (m);i >= (n);i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define pb(a) push_back(a)
+#define ALL(obj) (obj).begin(),(obj).end()
+
+using namespace std;
+
+const int INF = 99999999;
+const ll LINF = 9999999999999;
+
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+int vx[4] = { 0,1,0,-1 };
+int vy[4] = { 1,0,-1,0 };
+
+string s,s2;
+
+signed main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
+
+	cin >> s >> s2;
+	if (s[0] == s2[2] && s2[0] == s[2] && s[1] == s2[1]) {
+		cout << "YES";
+	}
+	else cout << "NO";
+
+
+	return 0;
+
+}
