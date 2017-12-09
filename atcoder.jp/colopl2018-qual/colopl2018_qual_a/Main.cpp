f4310b403a559a5948d3a63918194a966f38db69
DoubleZ <kohei.tashiro@chiba-u.jp>
2017-12-09 21:02:19 +0900

    [AC] colopl2018-qual colopl2018_qual_a

---
+++ atcoder.jp/colopl2018-qual/colopl2018_qual_a/Main.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#define REP(i,n) for(int i=0;i< (n);i++)
+#define REPR(i, n) for(int i = (n);i >= 0;i--)
+#define FOR(i, m, n) for(int i = (m);i < (n);i++)
+#define FORR(i, m, n) for(int i = (m);i >= (n);i--)
+#define SORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define pb(a) push_back(a)
+#define ALL(obj) (obj).begin(),(obj).end()
+
+using namespace std;
+
+const int INF = 99999999;
+const ll LINF = 1145141919810;
+
+
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+int vx[4] = { 0,1,0,-1 };
+int vy[4] = { 1,0,-1,0 };
+
+signed main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	string s;
+	int a, b;
+	cin >> a >> b >> s;
+	if (s.length() >= a && s.length() <= b) { cout << "YES"; }
+	else { cout << "NO"; }
+
+	return 0;
+}
